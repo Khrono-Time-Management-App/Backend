96ebb7790d8899c8c05fa603fad17d6212220ca8
MehiRobert <mrie2764@scs.ubbcluj.ro>
2021-11-10 23:49:30 +0200

    Added Jwt Authentication

--- src/main/java/com/khrono/app/service/user/IUserService.java
+++ src/main/java/com/khrono/app/service/user/IUserService.java
@@ -1,7 +1,14 @@
 package com.khrono.app.service.user;
 
 import com.khrono.app.domain.User;
+import com.khrono.app.dtos.UserPasswordDto;
+
+import java.util.List;
 
 public interface IUserService {
     User saveUser(User user);
+
+    List<User> getAllUsers();
+
+    UserPasswordDto findUser(UserPasswordDto user);
 }
--- src/main/java/com/khrono/app/service/user/UserServiceImplementation.java
+++ src/main/java/com/khrono/app/service/user/UserServiceImplementation.java
@@ -1,29 +1,52 @@
 package com.khrono.app.service.user;
 
 import com.khrono.app.domain.User;
+import com.khrono.app.dtos.UserPasswordDto;
 import com.khrono.app.repository.IUserRepository;
 import com.khrono.app.service.sequence.SequenceGenerator;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Base64;
+import java.util.List;
 
 @AllArgsConstructor
 @Service
 public class UserServiceImplementation implements IUserService{
 
-    @Autowired
-    private IUserRepository userRepository;
 
-    @Autowired
-    private SequenceGenerator sequenceGenerator;
+    private final IUserRepository userRepository;
+
+
+    private final SequenceGenerator sequenceGenerator;
+
+    private final PasswordEncoder encoder;
 
     @Override
     public User saveUser(User userEntity) {
-        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        userEntity.setPassword(passwordEncoder.encode(userEntity.getPassword()));
+        userEntity.setPassword(encoder.encode(userEntity.getPassword()));
         userEntity.setId(sequenceGenerator.getSequenceNumber(User.SEQUENCE_NAME));
         return userRepository.save(userEntity);
     }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public UserPasswordDto findUser(UserPasswordDto user) {
+        User foundUser = userRepository.findByEmail(user.getEmail());
+
+        if(foundUser == null)
+            return null;
+        else if (!encoder.matches(user.getPassword(), foundUser.getPassword())) {
+            user.setPassword(null);
+        }
+        return user;
+
+    }
+
+
 }
